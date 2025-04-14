ae999d213c0d757177c2fdb56ddd600bd6f624eb
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-04-15 00:37:52 +0100

    Imported the cart icon in the Header component

--- food-store/src/components/Header.js
+++ food-store/src/components/Header.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import Navbar from './Navbar'
 import './header.scss'
+import cart_icon from '../assets/icons/shopping-bag.png'
 
 const Header = () => {
     return (
         <header className='header-wrapper'>
             <div className='header-inner'>
                 <div className='header-logo'>
-                   <h1>Eatty<span>Filly</span></h1>
+                    <h1>Eatty<span>Filly</span></h1>
                 </div>
                 <Navbar />
-               
+                <div>
+                    <img src={cart_icon} alt='cart icon' />
+                </div>
+                <div>
+                    <h4>Login</h4>
+                </div>
             </div>
         </header>
     )
