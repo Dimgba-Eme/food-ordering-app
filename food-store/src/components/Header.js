c54829f031a54c69f1e3f70645be26aed6b4fc16
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-04-15 00:06:56 +0100

    Imported the Navbar component inside the Header file

--- food-store/src/components/Header.js
+++ food-store/src/components/Header.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import Navbar from './Navbar'
 
 const Header = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className='header-wrapper'>
+            <div className='header-inner'>
+
+                <Navbar />
+            </div>
+        </div>
+    )
 }
 
 export default Header
