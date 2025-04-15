913a1b497cbdee1cde0bb5c9a149ab1ef56640bf
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-04-15 20:04:27 +0100

    Imported the hero image in the Hero component

--- food-store/src/components/Hero.js
+++ food-store/src/components/Hero.js
@@ -1,11 +1,22 @@
 import React from 'react'
+import hero_pic from '../assets/images/hero_pic.jpg'
 
 const Hero = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <section className='hero-container'>
+            <div className='hero-inner'>
+                <div className='hero-intro-container'>
+                    <h1>Order your favourite delicacies</h1>
+                    <p>Choose from a diverse menu featuring a delectable array of dishes crafted
+                        with the finest ingredients and culinary expertise. Our mission is to
+                        satisfy your cravings and elevate your dining experience, one delicious meal at a time.
+                    </p>
+                </div>
+                <img src={hero_pic} alt='hero pic' className='hero-pic' />
+            </div>
+
+        </section>
+    )
 }
 
 export default Hero
