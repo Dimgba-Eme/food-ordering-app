3871abda9284dcd48e8a64c1551fd8c8ebcf085b
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-04-15 01:00:42 +0100

    Imported the menu icon in the Navbar component

--- food-store/src/components/Navbar.js
+++ food-store/src/components/Navbar.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './navbar.scss'
+import menu_icon from '../assets/icons/hamburger_menu.svg'
 
 const Navbar = () => {
     return (
         <>
             <nav className='nav-container'>
+                <img src={menu_icon} alt='menu icon' className='menu-icon' />
                 <ul className='nav-inner'>
                     <li>Home</li>
                     <li>About</li>
