8d36c1df1a8cdf2064eae634d745df36c20cd98f
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-04-16 12:48:38 +0100

    Added all the necessary food images in the images folder within the assets folder

---
+++ food-store/src/assets/images/all_menu.js
@@ -0,0 +1,384 @@
+import image1 from '../images/image1.jpeg'
+import image2 from '../images/image2.jpeg'
+import image3 from '../images/image3.jpeg'
+import image4 from '../images/image4.jpeg'
+import image5 from '../images/image5.jpeg'
+import image6 from '../images/image6.jpeg'
+import image7 from '../images/image7.jpeg'
+import image8 from '../images/image8.jpeg'
+import image9 from '../images/image9.jpeg'
+import image10 from '../images/image10.jpeg'
+import image11 from '../images/image11.jpeg'
+import image12 from '../images/image12.jpeg'
+import image13 from '../images/image13.jpeg'
+import image14 from '../images/image14.jpg'
+import image15 from '../images/image15.jpeg'
+import image16 from '../images/image16.jpeg'
+import image17 from '../images/image17.jpg'
+import image18 from '../images/image18.jpeg'
+import image19 from '../images/image19.jpeg'
+import image20 from '../images/image20.jpeg'
+import image21 from '../images/image21.jpeg'
+import image22 from '../images/image22.jpeg'
+import image23 from '../images/image23.jpg'
+import image24 from '../images/image24.jpg'
+import image25 from '../images/image25.jpg'
+import image26 from '../images/image26.jpg'
+import image27 from '../images/image27.jpg'
+import image28 from '../images/image28.jpg'
+import image29 from '../images/image29.jpg'
+import image30 from '../images/image30.jpg'
+import image31 from '../images/image31.jpg'
+import image32 from '../images/image32.jpeg'
+import image33 from '../images/image33.jpeg'
+import image34 from '../images/image34.jpeg'
+import image35 from '../images/image35.jpeg'
+import image36 from '../images/image36.jpeg'
+import image37 from '../images/image37.jpeg'
+import image38 from '../images/image38.jpeg'
+import image39 from '../images/image39.jpeg'
+import image40 from '../images/image40.jpeg'
+import image41 from '../images/image41.jpeg'
+import image42 from '../images/image42.jpeg'
+
+
+
+const all_menu = [
+    {
+        id: 1,
+        image: image1,
+        name: 'Cajun Shrimp and Salmon',
+        price: 12.00,
+        category: 'mains'
+    },
+
+    {
+        id: 2,
+        image: image2,
+        name: 'Chicken Chow Mein',
+        price: 10.00,
+        category: 'mains'
+    },
+
+    {
+        id: 3,
+        image: image3,
+        name: 'chicken and fries',
+        price: 12.00,
+        category: 'mains'
+    },
+
+    {
+        id: 4,
+        image: image4,
+        name: 'crack burgers',
+        price: 15.00,
+        category: 'mains'
+    },
+
+    {
+        id: 5,
+        image: image5,
+        name: 'Creamy Chicken Supreme',
+        price: 9.00,
+        category: 'mains'
+    },
+
+    {
+        id: 6,
+        image: image6,
+        name: 'Crispy Bang Salmon',
+        price: 15.00,
+        category: 'mains'
+    },
+
+    {
+        id: 7,
+        image: image7,
+        name: 'Garlic Butter Lobster Tails',
+        price: 12.00,
+        category: 'mains'
+    },
+
+    {
+        id: 8,
+        image: image8,
+        name: 'Garlic Butter Salmon',
+        price: 18.00,
+        category: 'mains'
+    },
+
+    {
+        id: 9,
+        image: image9,
+        name: 'Lime-Garlic Steamed Fish',
+        price: 17.00,
+        category: 'mains'
+    },
+
+    {
+        id: 10,
+        image: image10,
+        name: 'Pan-Seared Salmon',
+        price: 19.00,
+        category: 'mains'
+    },
+
+    {
+        id: 11,
+        image: image11,
+        name: 'Parmesan Crusted Chicken',
+        price: 20.00,
+        category: 'mains'
+    },
+
+    {
+        id: 12,
+        image: image12,
+        name: 'Pasta',
+        price: 18.00,
+        category: 'mains'
+    },
+
+    {
+        id: 13,
+        image: image13,
+        name: 'Pork Tenderloin',
+        price: 15.00,
+        category: 'mains'
+    },
+
+    {
+        id: 14,
+        image: image14,
+        name: 'Bruschetta',
+        price: 8.00,
+        category: 'appetizer'
+    },
+
+    {
+        id: 15,
+        image: image15,
+        name: 'Creamy Smothered Chicken',
+        price: 10.00,
+        category: 'appetizer'
+    },
+
+    {
+        id: 16,
+        image: image16,
+        name: 'Egg omelette',
+        price: 7.00,
+        category: 'appetizer'
+    },
+
+    {
+        id: 17,
+        image: image17,
+        name: 'Greek Salad',
+        price: 9.00,
+        category: 'appetizer'
+    },
+
+    {
+        id: 18,
+        image: image18,
+        name: 'Grilled Chicken',
+        price: 8.00,
+        category: 'appetizer'
+    },
+
+    {
+        id: 19,
+        image: image19,
+        name: 'Pork',
+        price: 7.00,
+        category: 'appetizer'
+    },
+
+    {
+        id: 20,
+        image: image20,
+        name: 'Stuffed Chicken Breast',
+        price: 10.00,
+        category: 'appetizer'
+    },
+
+    {
+        id: 21,
+        image: image21,
+        name: 'Stuffed Chicken',
+        price: 8.00,
+        category: 'appetizer'
+    },
+
+    {
+        id: 22,
+        image: image22,
+        name: 'Veggie Pie',
+        price: 10.00,
+        category: 'appetizer'
+    },
+
+    {
+        id: 23,
+        image: image23,
+        name: 'baklava',
+        price: 10.00,
+        category: 'dessert'
+    },
+
+    {
+        id: 24,
+        image: image24,
+        name: 'brownie',
+        price: 10.00,
+        category: 'dessert'
+    },
+
+    {
+        id: 25,
+        image: image25,
+        name: 'cake',
+        price: 10.00,
+        category: 'dessert'
+    },
+
+    {
+        id: 26,
+        image: image26,
+        name: 'creme-brulee',
+        price: 10.00,
+        category: 'dessert'
+    },
+
+    {
+        id: 27,
+        image: image27,
+        name: 'macaron',
+        price: 10.00,
+        category: 'dessert'
+    },
+
+    {
+        id: 28,
+        image: image28,
+        name: 'meringue',
+        price: 10.00,
+        category: 'dessert'
+    },
+
+    {
+        id: 29,
+        image: image29,
+        name: 'panna-cotta',
+        price: 10.00,
+        category: 'dessert'
+    },
+
+    {
+        id: 30,
+        image: image30,
+        name: 'tiramisu',
+        price: 10.00,
+        category: 'dessert'
+    },
+
+    {
+        id: 31,
+        image: image31,
+        name: 'waffle',
+        price: 10.00,
+        category: 'dessert'
+    },
+
+    {
+        id: 32,
+        image: image32,
+        name: '7UP Lemon',
+        price: 5.00,
+        category: 'drinks'
+    },
+
+    {
+        id: 33,
+        image: image33,
+        name: 'AQUA AIR MINERAL',
+        price: 4.00,
+        category: 'drinks'
+    },
+
+    {
+        id: 34,
+        image: image34,
+        name: 'choco smoothie',
+        price: 6.00,
+        category: 'drinks'
+    },
+
+    {
+        id: 35,
+        image: image35,
+        name: 'Coca-Cola',
+        price: 8.00,
+        category: 'drinks'
+    },
+
+    {
+        id: 36,
+        image: image36,
+        name: 'Favorite Soda',
+        price: 3.00,
+        category: 'drinks'
+    },
+
+    {
+        id: 37,
+        image: image37,
+        name: 'Homemade Fresh Lemonade',
+        price: 5.00,
+        category: 'drinks'
+    },
+
+    {
+        id: 38,
+        image: image38,
+        name: 'Sprite',
+        price: 4.00,
+        category: 'drinks'
+    },
+
+    {
+        id: 39,
+        image: image39,
+        name: 'Strawberry Banana Smoothie',
+        price: 6.00,
+        category: 'drinks'
+    },
+
+    {
+        id: 40,
+        image: image40,
+        name: 'Strawberry Lemon Smoothie',
+        price: 5.00,
+        category: 'drinks'
+    },
+
+    {
+        id: 41,
+        image: image41,
+        name: 'Suco Verde Detox ',
+        price: 4.00,
+        category: 'drinks'
+    },
+
+    {
+        id: 42,
+        image: image42,
+        name: 'Your Favorite Soda',
+        price: 7.00,
+        category: 'drinks'
+    },
+]
+
+export default all_menu;
---
+++ food-store/src/assets/images/specials.js
@@ -0,0 +1,45 @@
+import image4 from '../images/image4.jpeg'
+import image15 from '../images/image15.jpeg'
+import image20 from '../images/image20.jpeg'
+import image30 from '../images/image30.jpg'
+
+
+const specials = [
+    {
+        id: 4,
+        image: image4,
+        name: 'crack burgers',
+        description: 'Juicy, flavorful, and irresistibly good, this Crack Burger earns its name for being completely addictive',
+        price: 15.00,
+        category: 'mains'
+    },
+
+    {
+        id: 15,
+        image: image15,
+        name: 'Creamy Smothered Chicken',
+        description: 'These chicken thighs are smothered in the most heavenly gravy filled with peppers, onions, and mushrooms',
+        price: 10.00,
+        category: 'appetizer'
+    },
+
+    {
+        id: 20,
+        image: image20,
+        name: 'Stuffed Chicken Breast',
+        description: 'Cajun-seasoned chicken breasts stuffed with spinach, cottage cheese, and onions before being cooked in butter',
+        price: 10.00,
+        category: 'appetizer'
+    },
+
+    {
+        id: 30,
+        image: image30,
+        name: 'tiramisu',
+        description: 'Made the proper Italian way with mascarpone, raw eggs, coffee and liquor',
+        price: 10.00,
+        category: 'dessert'
+    },
+]
+
+export default specials;
