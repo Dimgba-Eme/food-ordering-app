59aa55ed10fd0627a0d7b9cbce09a514971db2fa
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-04-16 12:53:30 +0100

    Created specials.scss file and imported it to the Specials component for styling

--- food-store/src/pages/Home.js
+++ food-store/src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import Specials from '../components/Specials'
 import ExploreMenu from '../components/ExploreMenu'
 
 const Home = () => {
   return (
     <main>
       <Hero />
+      <Specials />
       <ExploreMenu />
     </main>
   )
