2cb354e9957888d62ca4e0c58e02f4928d25ab8b
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-04-16 13:06:24 +0100

    Imported the specials file from the assets folder into the Specials components for fetching special items

--- food-store/src/components/Specials.js
+++ food-store/src/components/Specials.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import './specials.scss'
+import specials from '../assets/images/specials'
 
 const Specials = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <section className='specials-container'>
+            <div className='specials-inner'>
+                <h1 className='specials-title'>This Week's Specials</h1>
+                <p className='specials-description'>Savor the extraordinary with our culinary masterpieces crafted to perfection</p>
+            </div>
+
+            <div className='specials-items-container'>
+              {specials.map((item, index) => {
+
+              })}
+            </div>
+        </section>
+    )
 }
 
 export default Specials
