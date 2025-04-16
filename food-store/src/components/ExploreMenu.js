e7154a1da93f5938e846300800070a41c126b90b
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-04-16 18:08:52 +0100

    Created and imported exploreMenu.scss file into the ExploreMenu component for styling

--- food-store/src/components/ExploreMenu.js
+++ food-store/src/components/ExploreMenu.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import './exploreMenu.scss'
 import menu_list from '../assets/images/menu_list'
+import bicycle from '../assets/images/bicycle.jpg'
 
 const ExploreMenu = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <section className='explore-menu-container'>
+            <h1 className='explore-menu-title'>Explore Our Menu</h1>
+            <div className='explore-order-container'>
+                <h4 className='explore-order'>Order for Delivery!</h4>
+                <img src={bicycle} alt='delivery bicycle' className='bicycle' />
+            </div>
+
+        </section>
+    )
 }
 
 export default ExploreMenu
