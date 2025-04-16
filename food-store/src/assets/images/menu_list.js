4bde9897f67550845c46f933dcdbc2d47c36710b
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-04-16 17:52:34 +0100

    Created the menu_list database where the menu list or category is fetched

---
+++ food-store/src/assets/images/menu_list.js
@@ -0,0 +1,30 @@
+import image1 from '../images/image1.jpeg'
+import image16 from '../images/image16.jpeg'
+import image24 from '../images/image24.jpg'
+import image42 from '../images/image42.jpeg'
+
+
+
+const menu_list = [
+    {
+        menu_image: image1,
+        menu_name: 'Mains',
+    },
+
+    {
+        menu_image: image16,
+        menu_name: 'Appetizers',
+    },
+
+    {
+        menu_image: image24,
+        menu_name: 'Desserts',
+    },
+
+    {
+        menu_image: image42,
+        menu_name: 'Drinks',
+    },
+]
+
+export default menu_list;
