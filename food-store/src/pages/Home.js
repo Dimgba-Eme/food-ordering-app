d861ae8c858dfe884af40bcd7bc1bbf4a32cc936
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-04-16 09:53:41 +0100

    Created ExploreMenu component and imported it in the Home page

--- food-store/src/pages/Home.js
+++ food-store/src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import ExploreMenu from '../components/ExploreMenu'
 
 const Home = () => {
   return (
     <main>
       <Hero />
+      <ExploreMenu />
     </main>
   )
 }
