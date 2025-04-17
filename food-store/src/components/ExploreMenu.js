81d5ba92f6cd6c2d7d21668976ff18d88d20b4b9
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-04-17 12:38:27 +0100

    Changed the ExploreMenu section background-color to #FBDABB. Also made all views responsive

--- food-store/src/components/ExploreMenu.js
+++ food-store/src/components/ExploreMenu.js
@@ -3,7 +3,8 @@ import './exploreMenu.scss'
 import menu_list from '../assets/images/menu_list'
 import bicycle from '../assets/images/bicycle.jpg'
 
-const ExploreMenu = () => {
+const ExploreMenu = (props) => {
+    const { category, setCategory } = props;
     return (
         <section className='explore-menu-container'>
             <h1 className='explore-menu-title'>Explore Our Menu</h1>
@@ -14,8 +15,8 @@ const ExploreMenu = () => {
 
             <div className='explore-menu'>
                 {menu_list.map((item, index) => {
-                    return <div key={index} className='explore-menu-inner'>
-                        <img src={item.menu_image} alt='menu item' className='menu-item-pic' />
+                    return <div key={index} className='explore-menu-inner' onClick={() => setCategory((prev) => prev === item.menu_name ? "All" : item.menu_name)}>
+                        <img src={item.menu_image} alt='menu item' className={category === item.menu_name ? "active" : ''} />
                         <h4 className='menu-item-name'>{item.menu_name}</h4>
                     </div>
                 })}
