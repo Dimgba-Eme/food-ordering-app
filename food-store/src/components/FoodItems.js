7f1a4d440d7aaf7ff38ce6ab6d2f167e429b49ab
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-04-17 16:15:52 +0100

    Used the ternary operator to add logic to the add to cart functionality

--- food-store/src/components/FoodItems.js
+++ food-store/src/components/FoodItems.js
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './foodItems.scss'
+import icon_add_to_cart from '../assets/icons/icon-add-to-cart.svg'
 
 const FoodItems = (props) => {
+    const [itemCount, setItemCount] = useState(0)
     return (
         <div className='food-items-container'>
             <div className='food-items-inner' key={props.id}>
                 <img src={props.image} alt='' />
-                <h1>{props.name}</h1>
-                <p>${props.price}</p>
+
+
+                {!itemCount
+                    ? <div className='add-to-cart-container'>
+                        <img src={icon_add_to_cart} alt='add to cart icon' className='add-to-cart-icon' />
+                        <p className='add-to-cart-title'>Add to cart</p>
+                    </div>
+                    : <div></div>}
+
+                <h1 className='item-title'>{props.name}</h1>
+                <p className='item-price'>${props.price}</p>
             </div>
 
         </div>
