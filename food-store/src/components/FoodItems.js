bad821609390f18cfd468537f5a79f1ef532841a
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-04-17 13:21:57 +0100

    Created foodItems.scss file and imported it into the FoodItems file for styling

--- food-store/src/components/FoodItems.js
+++ food-store/src/components/FoodItems.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import './foodItems.scss'
 
-const FoodItems = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const FoodItems = (props) => {
+    return (
+        <div className='food-items-container'>
+            <div className='food-items-inner' key={props.id}>
+                <img src={props.image} alt='' />
+                <h1>{props.name}</h1>
+                <p>{props.price}</p>
+            </div>
+
+        </div>
+    )
 }
 
 export default FoodItems
