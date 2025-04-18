c94f80b1c8b33cd0d52f37fc665a9c89415d2646
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-04-18 13:54:39 +0100

    Created getTotalCartAmount function in the ContextProvider component

--- food-store/src/contexts/ContextProvider.js
+++ food-store/src/contexts/ContextProvider.js
@@ -17,6 +17,19 @@ const ContextProvider = ({ children }) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
   }
 
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let cartInfo = all_menu.find((i) => i.id === Number(item));
+        totalAmount += cartInfo.price * cartItems[item]
+      }
+    }
+    return totalAmount;
+  }
+
+  
+
   const contextValue = { all_menu, cartItems, setCartItems, addToCart, removeFromCart }
 
   return (
