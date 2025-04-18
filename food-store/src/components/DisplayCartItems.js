74cf53d85f2e3c6816b11a88cfc8d6dff7ed6f07
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-04-18 17:13:01 +0100

    Imported the ContextProvider values to the DisplayCartItems component for use

--- food-store/src/components/DisplayCartItems.js
+++ food-store/src/components/DisplayCartItems.js
@@ -1,12 +1,34 @@
 import React from 'react'
-import './displayCartItem.scss'
+import './displayCartItems.scss'
+import { useItems } from '../contexts/ContextProvider'
 
 const DisplayCartItems = () => {
+    const {all_menu, cartItems, getTotalCartAmount, getTotalCartItems, removeFromCart} = useItems();
     return (
         <section className='cart-items-container'>
-          <div className='cart-items-inner'>
+            <div className='cart-items-inner'>
+                <div className='cart-items-info'>
+                    <p>Product</p>
+                    <p>Title</p>
+                    <p>Price</p>
+                    <p>Quantity</p>
+                    <p>Total</p>
+                    <p>Remove</p>
+                </div>
 
-          </div>
+                <div className='cart-items-added'>
+                    <img />
+                    <h4>item title</h4>
+                    <p>item price</p>
+                    <div className='cart-item-quantity-container'>
+                        <p>minus</p>
+                        <p>number</p>
+                        <p>plus</p>
+                    </div>
+                    <h4>item total</h4>
+                    <img />
+                </div>
+            </div>
         </section>
     )
 }
