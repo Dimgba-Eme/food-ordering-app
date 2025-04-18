c4121ac9f1de4501ae5eb2f89ca2cc568a0fd869
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-04-18 13:36:31 +0100

    The addToCart and removeFromCart functions and working as expected in the FoodItems component

--- food-store/src/components/FoodItems.js
+++ food-store/src/components/FoodItems.js
@@ -1,29 +1,30 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './foodItems.scss'
 import icon_add_to_cart from '../assets/icons/icon-add-to-cart.svg'
 import icon_decrement_quantity from '../assets/icons/icon-decrement-quantity.svg'
 import icon_increment_quantity from '../assets/icons/icon-increment-quantity.svg'
 import { useItems } from '../contexts/ContextProvider'
 
-const {cartItems, setCartItems, addToCart, removeFromCart} = useItems();
 
 const FoodItems = (props) => {
-    const [itemCount, setItemCount] = useState(0)
+
+    const { cartItems, addToCart, removeFromCart } = useItems();
+
     return (
         <div className='food-items-container'>
             <div className='food-items-inner' key={props.id}>
                 <img src={props.image} alt='' />
 
 
-                {!itemCount
-                    ? <div className='add-to-cart-container' onClick={() => setItemCount(prev => prev + 1)}>
+                {!cartItems[props.id]
+                    ? <div className='add-to-cart-container' onClick={() => addToCart(props.id)}>
                         <img src={icon_add_to_cart} alt='add to cart icon' className='add-to-cart-icon' />
                         <p className='add-to-cart-title'>Add to cart</p>
                     </div>
                     : <div className='cart-quantity-container'>
-                        <img src={icon_decrement_quantity} alt='icon decrement' className='icon-plus-minus' onClick={() => setItemCount(prev => prev - 1)} />
-                        <p className='item-count'>{itemCount}</p>
-                        <img src={icon_increment_quantity} alt='icon decrement' className='icon-plus-minus' onClick={() => setItemCount(prev => prev + 1)} />
+                        <img src={icon_decrement_quantity} alt='icon decrement' className='icon-plus-minus' onClick={() => removeFromCart(props.id)} />
+                        <p className='item-count'>{cartItems[props.id]}</p>
+                        <img src={icon_increment_quantity} alt='icon decrement' className='icon-plus-minus' onClick={() => addToCart(props.id)} />
                     </div>}
 
                 <h1 className='item-title'>{props.name}</h1>
--- food-store/src/components/Navbar.js
+++ food-store/src/components/Navbar.js
@@ -3,9 +3,11 @@ import './navbar.scss'
 import menu_icon from '../assets/icons/hamburger_menu.svg'
 import cart_icon from '../assets/icons/shopping-bag.png'
 import close_icon from '../assets/icons/close_icon.png'
+import { useItems } from '../contexts/ContextProvider'
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const { addToCart } = useItems();
     return (
         <>
             <img src={menu_icon} alt='menu icon' className='menu-icon' onClick={() => setIsOpen(!isOpen)} />
@@ -19,10 +21,10 @@ const Navbar = () => {
                     </ul>
                 </nav>
             </div>
-                <div className='cart-icon-container'>
-                    <img src={cart_icon} alt='cart icon' className='cart-icon' />
-                    <p className='item-count'>0</p>
-                </div>
+            <div className='cart-icon-container'>
+                <img src={cart_icon} alt='cart icon' className='cart-icon' />
+                <p className='item-count'>{addToCart()}</p>
+            </div>
 
         </>
     )
