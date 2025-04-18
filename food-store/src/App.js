52752a8ecf750d1393c86a510568af73d85edb81
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-04-18 14:37:52 +0100

    Imported the Cart page in the App.js file

--- food-store/src/App.js
+++ food-store/src/App.js
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Cart from "./pages/Cart";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -12,6 +13,7 @@ function App() {
       <div>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
         </Routes>
 
       </div>
