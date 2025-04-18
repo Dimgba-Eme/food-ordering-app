7e1f392a9f04e42ecc2070b687a607ed0ed74b69
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-04-18 14:36:31 +0100

    Created the Cart page

--- food-store/src/pages/Cart.js
+++ food-store/src/pages/Cart.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const About = () => {
+const Cart = () => {
   return (
     <div>
-      
+      <p>Cart</p>
     </div>
   )
 }
 
-export default About
+export default Cart
