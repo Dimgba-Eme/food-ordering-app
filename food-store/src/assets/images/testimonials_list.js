990d4870b6f3479685269796f82c31c26b4c3363
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-04-19 22:50:43 +0100

    Imported the images and icon needed in the testimonials_list file

--- food-store/src/assets/images/testimonials_list.js
+++ food-store/src/assets/images/testimonials_list.js
@@ -1,4 +1,8 @@
+import user_one from '../images/user-1.jpg'
+import user_two from '../images/user-2.jpg'
+import user_three from '../images/user-3.jpg'
 
+import rating from '../icons/rating.png'
 
 
 
@@ -7,23 +11,23 @@
 
 const testimonials_list = [
     {
-        image: '',
-        name: "Ugo Dimgba",
-        rating: "",
+        image: user_one,
+        name: "Dimson Black",
+        rating: rating,
         comment: "I ordered my breakfast this morning and dinner tonight from EattyFilly.Delivery was fast and the food still hot.",
     },
 
     {
-        image: '',
-        name: "Onwuka Ina",
-        rating: "",
+        image: user_two,
+        name: "Amber Israel",
+        rating: rating,
         comment: "This restaurant has left the best impressions. Delicious dishes, beautiful packaging, quick delivery and the best dessert i've ever had. I recommend to everyone!",
     },
 
     {
-        image: '',
-        name: "Ifebueme Chika",
-        rating: "",
+        image: user_three,
+        name: "Ruby Jasper",
+        rating: rating,
         comment: "Do yourself a favor and order from EattyFilly. The service is unmatched. The food is absolutely amazing. Considering the quality, the price is reasonable.",
     },
 ]
