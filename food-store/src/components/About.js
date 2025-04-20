2d6b968382892b34dea3948457e8ee63022d4803
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-04-20 13:41:52 +0100

    Styled and aligned the About section in the Home page. Also responsive in all views

--- food-store/src/components/About.js
+++ food-store/src/components/About.js
@@ -8,7 +8,11 @@ const About = () => {
             <h1>About Us</h1>
             <div className='about-inner'>
                 <img src={about_image} alt='about image' />
-                <p></p>
+                <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.
+                    Our story is one of passion, dedication, and a commitment to delivering an exceptional culinary experience
+                    to our cherished customers. At EattyFilly, our journey began with a simple yet profound vision - to create a haven for food enthusiasts,
+                    a place where flavors come alive, with the aim of offering a unique blend of delectable cuisine, inviting ambiance, and impeccable service.
+                </p>
             </div>
         </div>
     )
