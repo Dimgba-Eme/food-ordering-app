345edc95e46b64166a848d115337e3f1c9cff4c9
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-04-20 14:10:24 +0100

    Created footer.scss file and imported it into the Footer component for styling

--- food-store/src/components/Footer.js
+++ food-store/src/components/Footer.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <footer>
-      
+    <footer id='footer' className='footer-container'>
+
+      <div className='footer-inner'>
+        <div className='logo-container'>
+          <Link to='/'><h1 className='eatty'>Eatty<span className='filly'>Filly</span></h1></Link>
+        </div>
+
+        <div className='contact-container'>
+          <h1>Contact Us</h1>
+          <p>Phone: +234 708 549 5430</p>
+          <p>Email: hello@dimgbaeme.com.ng</p>
+        </div>
+      </div>
+
+      <p className='rights'>All rights reserved by EattyFilly, 2025</p>
+
+
+
     </footer>
   )
 }
