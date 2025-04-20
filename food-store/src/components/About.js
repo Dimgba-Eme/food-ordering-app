0c051a243b562f5352fbc856fae6c92fd246acca
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-04-20 12:57:39 +0100

    Imported the about_image for the about us section in the Home page

--- food-store/src/components/About.js
+++ food-store/src/components/About.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import './about.scss'
+import about_image from '../assets/images/about_image.jpg'
 
 const About = () => {
-  return (
-    <div id='about-us' className='about-container'>
-      <h1>About Us</h1> 
-    </div>
-  )
+    return (
+        <div id='about-us' className='about-container'>
+            <h1>About Us</h1>
+            <div className='about-inner'>
+                <img src={about_image} alt='about image' />
+                <p></p>
+            </div>
+        </div>
+    )
 }
 
 export default About
